4bc5e2cf9e8372e9f8cce9175785822e9bef54da
Nico Hinderling <nicolas.hinderling@gmail.com>
2017-01-28 20:17:02 -0500

    updates so far... basic as fuckkkk dood

--- Trampoline/GameScene.swift
+++ Trampoline/GameScene.swift
@@ -7,83 +7,76 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let player = SKSpriteNode(imageNamed: "hero")
+    let rightButton = SKSpriteNode(imageNamed: "right-button")
+    
+    var leftButtonPressed = false
+    var rightButtonPressed = false
     
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.white
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.6)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        addChild(player)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        addControls()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        //        physicsWorld.gravity = CGVector.zero
+        //        physicsWorld.contactDelegate = self
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    
+    
+    func addControls() {
+        rightButton.position = CGPoint(x:self.frame.midX * 0.2, y:self.frame.midY * 0.2)
+        
+        self.addChild(rightButton)
+        
+        
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    func moveHero() {
+        guard !(leftButtonPressed && rightButtonPressed) else { return }
+        
+        //        while (leftButtonPressed || rightButtonPressed) {
+        if leftButtonPressed {
+            print("smash the fuck outta that left button")
+        } else {
+            let actionMove = SKAction.move(to: CGPoint(x: player.position.x + size.width * 0.1, y: player.position.y), duration: TimeInterval(0.1))
+            player.run(SKAction.sequence([actionMove]))
+            moveHero()
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        //        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
+        if rightButton.contains(touchLocation) {
+            print("Right button pressed!")
+            rightButtonPressed = true
+            moveHero()
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
+        if rightButton.contains(touchLocation) {
+            print("Right button no longer pressed.")
+            rightButtonPressed = false
+            moveHero()
+        }
+        
     }
 }
